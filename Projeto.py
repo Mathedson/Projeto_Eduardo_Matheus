de50137c37d8ac81bdb0e1b45b92d3b98b0b2c0c
Eduardo <eduardovdlfg@gmail.com>
2019-09-05 16:48:47 -0300

    Parte Eduardo: classe Pessoa

--- Projeto.py
+++ Projeto.py
@@ -1,32 +1,17 @@
 from random import *
+from numpy import *
+class Pessoa():
+    def __init__(self):
+        self.modalidade = random.multinomial(100,[0.654 + random.uniform(-0.008,0.008),0.6])[0] #ocupado
+        self.ensino     = random.multinomial(100,[0.22 + random.uniform(-0.081,0.081),0.6])[0] #publico
+        self.sexo       = random.multinomial(100,[0.428 + random.uniform(-0.011,0.011),0.6])[0] #homem
+        self.cor        = random.multinomial(100,[0.421 + random.uniform(-0.013,0.013),0.6])[0] #preto ou pardo
+        self.modalidade = random.multinomial(100,[0.794 + random.uniform(-0.018,0.018),0.6])[0] #presencial
+        self.classificacao()
 
-class Pessoa:
-    def __init__(self, sexo, regiao, ocupaçao):
-        self.sexo     = idade
-        self.regiao   = regiao
-        self.ocupaçao = ocupaçao
-
-populaçao = [Pessoa(randrange(17,30), randrange(50,150)) for i in range(1000)]
-print(populaçao[0].peso, populaçao[0].idade)
-
-class População:
-    def __init__(self, tamanho=1000):
-        self.tamanho = tamanho
-        self.individuo = []
-pop = População()
-
-n = 'norte'
-ne = 'nordeste'
-co = 'centrooeste'
-se = 'sudeste'
-s = 'sul'
-regiao = [[n], [ne], [co], [se], [s]]
-
-for i in range(1, pop.tamanho +1):
-    rand= random(0,1)
-        if rand<= 269/478
-
-
-
-
-
+    def classificacao(self):
+        self.modalidade = 'Ocupado' if self.modalidade < random.uniform(0,100) else 'Não ocupado'
+        self.ensino= 'Público' if self.modalidade < random.uniform(0,100) else 'Privado'
+        self.sexo= 'Homem' if self.sexo < random.uniform(0,100) else 'Mulher'
+        self.cor = 'Preta ou parda' if self.cor < random.uniform(0,100) else 'Branca'
+        self.modalidade = 'Homem' if self.modalidade < random.uniform(0,100) else 'À distância'

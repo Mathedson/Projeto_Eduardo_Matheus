1dd99a64c60c0881b5de1550c266231dc8fdf75f
eduardovdlfg <eduardovdlfg@gmail.com>
2019-09-05 14:03:19 -0300

    inicio

---
+++ Projeto.py
@@ -0,0 +1,32 @@
+from random import *
+
+class Pessoa:
+    def __init__(self, sexo, regiao, ocupaçao):
+        self.sexo     = idade
+        self.regiao   = regiao
+        self.ocupaçao = ocupaçao
+
+populaçao = [Pessoa(randrange(17,30), randrange(50,150)) for i in range(1000)]
+print(populaçao[0].peso, populaçao[0].idade)
+
+class População:
+    def __init__(self, tamanho=1000):
+        self.tamanho = tamanho
+        self.individuo = []
+pop = População()
+
+n = 'norte'
+ne = 'nordeste'
+co = 'centrooeste'
+se = 'sudeste'
+s = 'sul'
+regiao = [[n], [ne], [co], [se], [s]]
+
+for i in range(1, pop.tamanho +1):
+    rand= random(0,1)
+        if rand<= 269/478
+
+
+
+
+
